898ad7dfc6916f48e8a74fa134b9c9fed245b9a2
Brandon Pineda <bpinedad96@gmail.com>
2022-03-08 14:25:08 -0600

    Added boost and audio
    Added boost function - Player will bend a little when doing this
    Added track audio
    Added crash audio
    Added win audio

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,16 +6,34 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb2d;
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 20f;
+    [SerializeField] GameObject playerTop;
+    SurfaceEffector2D surfaceEffector2D;
+
+    float jumpAccum = 0f;
+    [SerializeField] float jumpStep = 0.01f;
+    [SerializeField] float jumpMax = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
+        surfaceEffector2D.speed = baseSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (FindObjectOfType<CrashDetector>().isAlive) {
+            RotatePlayer();
+            RespondToBoost();
+            //RespondToJump();
+        }
+    }
+
+    void RotatePlayer (){
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb2d.AddTorque(torqueAmount);
@@ -24,6 +42,31 @@ public class PlayerController : MonoBehaviour
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             rb2d.AddTorque(-torqueAmount);
+        }
+    }
+
+    void RespondToBoost() {
+        //if we push up speed up, if not remain
+        if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            surfaceEffector2D.speed = boostSpeed;
+            playerTop.transform.Rotate(0, 0, -15f);
+        }
+        else if (Input.GetKeyUp(KeyCode.DownArrow)) {
+            surfaceEffector2D.speed = baseSpeed;
+            playerTop.transform.Rotate(0, 0, 15f);
+        }
+    }
+
+    void RespondToJump() {
+        //if we push up speed up, if not remain
+        if (Input.GetKey(KeyCode.UpArrow) && jumpAccum < jumpMax) {
+            jumpAccum += jumpStep;
+            transform.Translate(0, jumpStep, 0);
+        }
+        //Key released
+        if (Input.GetKeyUp(KeyCode.UpArrow)) {
+            //Avoid jumping again
+            jumpAccum = 5;
         }
     }
 }
